9b16f56b4d9375503fc735793ffca1e85b5e4cc7
diak <diak@mesterlevel.lo>
2025-03-04 12:31:14 +0100

    változtatás az app.js-ben

--- app.js
+++ app.js
@@ -110,8 +110,7 @@ app.use(cookieParser());
 app.use(cors({
     origin:'https://revyn.netlify.app',
     credentials: true, 
-    methods:['GET','POST','PUT','DELETE'],
-    allowedHeaders:['Content-Type','Authorization']
+    
 }));
 
 app.use('/uploads', authenticateToken, express.static(path.join(__dirname, 'uploads')));
